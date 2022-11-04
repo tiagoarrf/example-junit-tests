f5fb7313210147664bbbb17a47b824ce8d30348c
Tiago A. Fontenele <tiagoarrfon@gmail.com>
2022-11-03 21:48:38 -0300

    upload files initial

---
+++ src/test/java/com/example/testes/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package com.example.testes;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+    
+    @Test
+    public void testSoma() {
+        
+    }
+}
---
+++ src/test/java/com/example/testes/models/CalculadoraTest.java
@@ -0,0 +1,23 @@
+package com.example.testes.models;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;    
+    
+public class CalculadoraTest {
+
+    @Before
+    public void setup(){
+
+    }
+        
+    @Test
+    public void test() {
+        Calculadora calc = new Calculadora();
+        int soma = calc.somar("1+1+3");
+        assertEquals(5, soma); 
+    }
+}
+    
